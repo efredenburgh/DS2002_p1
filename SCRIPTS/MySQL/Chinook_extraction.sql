672f1c606cf3805cdcbb6b7fb72e2207462b9bb3
efredenburgh <157047455+efredenburgh@users.noreply.github.com>
2024-11-11 13:48:18 -0500

    Update Chinook_extraction.sql

--- SCRIPTS/MySQL/Chinook_extraction.sql
+++ SCRIPTS/MySQL/Chinook_extraction.sql
@@ -1,17 +1,17 @@
-# exporting to json and mongodb
+-- exporting to json and mongodb
 -- These are the small bits of code I used to extract data from each table in chinook for use in the chinook_dw database. I extracted Employee and Customer data as JSON files for use
 -- in MongoDB, and I extracted Invoice and InvoiceLine as CSV files that I turned into pandas dataframes.
 
 use chinook;
 
-SELECT * FROM Customer;
+SELECT * FROM Customer; -- selects everything from the customer table
 
 use chinook;
 
-SELECT * FROM Invoice;
+SELECT * FROM Invoice; -- selects everything from the invoice table
 
 use chinook;
-SELECT * FROM Employee;
+SELECT * FROM Employee; -- selects everything from the employee table
 
 use chinook; 
-SELECT * FROM InvoiceLine;
+SELECT * FROM InvoiceLine; -- selects everything from the invoice line table
